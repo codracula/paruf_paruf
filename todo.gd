ee1e596b0ca153d17e0d6aaf249e7df6edffd5fa
Jeep Naarkom <maimalai@gmail.com>
2023-10-06 00:09:38 -0700

    update title screen, option, continue menu

--- todo.gd
+++ todo.gd
@@ -3,12 +3,12 @@
 #2. work on animation idle   -done
 #3. work on animation move   -done
 #4. work on climb (use move anim)
-#5. work on title card. 
+#5. work on title card. -DONE
 #6. work on death and respawn
 #7. work on live count
 #8. work on lvl 1 rough design
-#9. work on start menu and play Button
-#10. work on continue button 
+#9. work on start menu and play Button   -done
+#10. work on continue button    -done
 #11. jump sound
 #12. death sound 
 #13. title card music
